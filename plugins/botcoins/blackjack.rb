4bad8a03dc839dda52ab5622fa04bb04381cdba5
Guy <ib@kshash.com>
2014-02-02 23:12:38 -0500

    omg colors

--- plugins/botcoins/blackjack.rb
+++ plugins/botcoins/blackjack.rb
@@ -9,10 +9,11 @@ class BlackJack
 
 		# our decks of cards
 		@deck = [
-			"A\u2665", "2\u2665", "3\u2665", "4\u2665", "5\u2665", "6\u2665", "7\u2665", "8\u2665", "9\u2665", "10\u2665", "J\u2665", "Q\u2665", "K\u2665", # Hearts
-			"A\u2663", "2\u2663", "3\u2663", "4\u2663", "5\u2663", "6\u2663", "7\u2663", "8\u2663", "9\u2663", "10\u2663", "J\u2663", "Q\u2663", "K\u2663", # Clubs
-			"A\u2660", "2\u2660", "3\u2660", "4\u2660", "5\u2660", "6\u2660", "7\u2660", "8\u2660", "9\u2660", "10\u2660", "J\u2660", "Q\u2660", "K\u2660", # Spades
-			"A\u2666", "2\u2666", "3\u2666", "4\u2666", "5\u2666", "6\u2666", "7\u2666", "8\u2666", "9\u2666", "10\u2666", "J\u2666", "Q\u2666", "K\u2666"] # Diamonds
+			["A", "04,00A\u2665\u000F"], [2, "04,002\u2665\u000F"], [3, "04,003\u2665\u000F"], [4, "04,004\u2665\u000F"], [5, "04,005\u2665\u000F"], [6, "04,006\u2665\u000F"], [7, "04,007\u2665\u000F"], [8, "04,008\u2665\u000F"], [9, "04,009\u2665\u000F"], [10, "04,0010\u2665\u000F"], ["J", "04,00J\u2665\u000F"], ["Q", "04,00Q\u2665\u000F"], ["K", "04,00K\u2665\u000F"], # Hearts
+			["A", "01,00A\u2663\u000F"], [2, "01,002\u2663\u000F"], [3, "01,003\u2663\u000F"], [4, "01,004\u2663\u000F"], [5, "01,005\u2663\u000F"], [6, "01,006\u2663\u000F"], [7, "01,007\u2663\u000F"], [8, "01,008\u2663\u000F"], [9, "01,009\u2663\u000F"], [10, "01,0010\u2663\u000F"], ["J", "01,00J\u2663\u000F"], ["Q", "01,00Q\u2663\u000F"], ["K", "01,00K\u2663\u000F"], # Clubs
+			["A", "01,00A\u2660\u000F"], [2, "01,002\u2660\u000F"], [3, "01,003\u2660\u000F"], [4, "01,004\u2660\u000F"], [5, "01,005\u2660\u000F"], [6, "01,006\u2660\u000F"], [7, "01,007\u2660\u000F"], [8, "01,008\u2660\u000F"], [9, "01,009\u2660\u000F"], [10, "01,0010\u2660\u000F"], ["J", "01,00J\u2660\u000F"], ["Q", "01,00Q\u2660\u000F"], ["K", "01,00K\u2660\u000F"], # Spades
+			["A", "04,00A\u2666\u000F"], [2, "04,002\u2666\u000F"], [3, "04,003\u2666\u000F"], [4, "04,004\u2666\u000F"], [5, "04,005\u2666\u000F"], [6, "04,006\u2666\u000F"], [7, "04,007\u2666\u000F"], [8, "04,008\u2666\u000F"], [9, "04,009\u2666\u000F"], [10, "04,0010\u2666\u000F"], ["J", "04,00J\u2666\u000F"], ["Q", "04,00Q\u2666\u000F"], ["K", "04,00K\u2666\u000F"]] # Diamonds
+
 	end
 
 
@@ -22,13 +23,13 @@ class BlackJack
 		total = 0
 
 		hand.each do |card|
-			if card[0] =~ /^A/
+			if card[0] == "A"
 				aces += 1
 				total += 11
 			elsif card[0] =~ /^[JQK]/
 				total += 10
-			elsif card[0] =~ /^\d+/
-				total += card.gsub(/\\u\d+$/i, '').to_i
+			else
+				total += card[0]
 			end
 		end
 
@@ -41,6 +42,29 @@ class BlackJack
 		return total
 	end
 
+	# Counts aces in your hand (WIP)
+	def aces_count(hand)
+		aces = 0
+
+		hand.each do |card|
+			if card[0] == "A"
+				aces += 1
+			end
+		end
+
+		return aces
+	end
+
+	# Renders your hand
+	def show_hand(hand)
+		showing_hand = ""
+
+		hand.each do |card|
+			showing_hand = showing_hand + card[1].to_s + " "
+		end
+
+		return showing_hand
+	end
 
 	# Start a game
 	match /blackjack (\d+)/i, method: :blackjack_bet, :react_on => :channel
@@ -50,6 +74,11 @@ class BlackJack
 
 		@hand.delete(m.user.nick)
 		bet = bet.to_i
+
+		if (bet > 300) or (bet < 10)
+			m.user.notice "Place a bet between 10 to 300 please."
+			return
+		end
 
 		if $DataBase['users'].find{ |h| h['nick'] == m.user.authname.downcase }
 			if ($DataBase['users'].find{ |h| h['nick'] == m.user.nick.downcase }['botcoins'] < bet)
@@ -65,7 +94,7 @@ class BlackJack
 				$DataBase['users'].find{ |h| h['nick'] == m.user.nick.downcase }['botcoins'] -= bet
 			end
 			save_DB
-			m.user.notice "Your hand: %s (%s). Dealer's hand: %s." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'][0] ]
+			m.user.notice "Your hand: %s(%s). Dealer's hand: %s." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'][0][1] ]
 		end
 	end
 
@@ -80,10 +109,10 @@ class BlackJack
 		@hand[m.user.nick]["player"] << @deck[0]
 
 		if hand_count(@hand[m.user.nick]["player"]) > 21
-			m.user.notice "BUST! Your hand: %s (%s)." % [@hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player'])]
+			m.reply "#{m.user.nick}: Better luck next time\u000F | Your hand: %s(%s)." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']) ]
 			@hand.delete(m.user.nick)
 		else
-			m.user.notice "Your hand: %s (%s). Dealer's hand: %s." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'][0] ]
+			m.user.notice "Your hand: %s(%s). Dealer's hand: %s." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'][0][1] ]
 		end
 	end
 
@@ -91,7 +120,7 @@ class BlackJack
 	match /stay/i, method: :blackjack_stay, :react_on => :channel
 	def blackjack_stay (m)
 		return if ignore_nick(m.user.nick) # ignore ignored users
-		return if (@hand.key?(m.user.nick) == false)	# ignore users who don't have a hand
+		return if (@hand.key?(m.user.nick) == false) # ignore users who don't have a hand
 
 		q = 0
 		@deck = @deck.shuffle
@@ -99,28 +128,34 @@ class BlackJack
 		dealer_hand = hand_count(@hand[m.user.nick]["dealer"])
 		player_hand = hand_count(@hand[m.user.nick]["player"])
 
+		# Dealer hits until they have over 17
 		while (dealer_hand < 17)
 			@hand[m.user.nick]["dealer"] << @deck[q]
 			dealer_hand = hand_count(@hand[m.user.nick]["dealer"])
 			q += 1
 		end
 
+		# Dealer Bust
 		if dealer_hand > 21
-			m.user.notice "DEALER BUST! Your hand: %s (%s). Dealer's hand: %s (%s)." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'], hand_count(@hand[m.user.nick]['dealer'])]
+			m.reply "#{m.user.nick}: Dealer bust\u000F | Your hand: %s(%s). Dealer's hand: %s(%s)." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), show_hand(@hand[m.user.nick]['dealer']), hand_count(@hand[m.user.nick]['dealer']) ]
 			$DataBase['users'].find{ |h| h['nick'] == m.user.nick.downcase }['botcoins'] += (@hand[m.user.nick]["bet"] * 2)
 			@hand.delete(m.user.nick)
 		
+		# Tie
 		elsif dealer_hand == player_hand
-			m.user.notice "TIE! Your hand: %s (%s). Dealer's hand: %s (%s)." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'], hand_count(@hand[m.user.nick]['dealer'])]
+			m.reply "#{m.user.nick}: Draw\u000F | Your hand: %s(%s). Dealer's hand: %s(%s)." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), show_hand(@hand[m.user.nick]['dealer']), hand_count(@hand[m.user.nick]['dealer']) ]
 			$DataBase['users'].find{ |h| h['nick'] == m.user.nick.downcase }['botcoins'] += @hand[m.user.nick]["bet"]
 			@hand.delete(m.user.nick)
 		
+		# Player wins
 		elsif player_hand > dealer_hand
-			m.user.notice "YOU WIN! Your hand: %s (%s). Dealer's hand: %s (%s)." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'], hand_count(@hand[m.user.nick]['dealer'])]
+			m.reply "#{m.user.nick}: You win\u000F | Your hand: %s(%s). Dealer's hand: %s(%s)." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), show_hand(@hand[m.user.nick]['dealer']), hand_count(@hand[m.user.nick]['dealer']) ]
 			$DataBase['users'].find{ |h| h['nick'] == m.user.nick.downcase }['botcoins'] += (@hand[m.user.nick]["bet"] * 2)
 			@hand.delete(m.user.nick)
+
+		# Player loses
 		else 
-			m.user.notice "DEALER WINS! Your hand: %s (%s). Dealer's hand: %s (%s)." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'], hand_count(@hand[m.user.nick]['dealer'])]
+			m.reply "#{m.user.nick}: Better luck next time\u000F | Your hand: %s(%s). Dealer's hand: %s(%s)." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), show_hand(@hand[m.user.nick]['dealer']), hand_count(@hand[m.user.nick]['dealer']) ]
 			@hand.delete(m.user.nick)
 		end
 
@@ -131,7 +166,7 @@ class BlackJack
 	match /surrender/i, method: :blackjack_surrender, :react_on => :channel
 	def blackjack_surrender (m)
 		return if ignore_nick(m.user.nick) # ignore ignored users
-		return if (@hand.key?(m.user.nick) == false)	# ignore users who don't have a hand
+		return if (@hand.key?(m.user.nick) == false) # ignore users who don't have a hand
 
 		m.user.notice "Better luck next time!"
 		$DataBase['users'].find{ |h| h['nick'] == m.user.nick.downcase }['botcoins'] += (@hand[m.user.nick]["bet"] / 2)
@@ -144,9 +179,9 @@ class BlackJack
 	match /hand/i, method: :blackjack_show_hand, :react_on => :channel
 	def blackjack_show_hand (m)
 		return if ignore_nick(m.user.nick) # ignore ignored users
-		return if (@hand.key?(m.user.nick) == false)	# ignore users who don't have a hand
+		return if (@hand.key?(m.user.nick) == false) # ignore users who don't have a hand
 
-		m.user.notice "Your hand: %s (%s). Dealer's hand: %s." % [ @hand[m.user.nick]['player'], hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'][0] ]
+		m.user.notice "Your hand: %s(%s). Dealer's hand: %s." % [ show_hand(@hand[m.user.nick]['player']), hand_count(@hand[m.user.nick]['player']), @hand[m.user.nick]['dealer'][0][1] ]
 	end
 
 end
